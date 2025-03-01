651049710822fea229ab75af1bbb06429fafa571
gaunt123 <ebojhak@gmail.com>
2025-03-01 23:50:44 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+function tictactoe(el){
+    window.location.href = "fractions.html"
+}
+var count=0;
+function next(el){
+    count+=1;
+    if(count>=3){
+        count=0;
+    }
+    document.getElementById('imageID').src = pic[count];
+    return count;
+}
+function aserai(el){
+    window.location.href = "aserai.html"
+}
+function battan(el){
+    window.location.href = "battan.html"
+}
+function sturgia(el){
+    window.location.href = "sturgia.html"
+}
+function imperia(el){
+    window.location.href = "imperia.html"
+}
+function khuzait(el){
+    window.location.href = "khuzait.html"
+}
+function vlandia(el){
+    window.location.href = "vlandia.html"
+}
+var pic=["sources/2.jpg","sources/3.jpg","sources/4.jpg"]
+function aserai(el){
+    window.location.href = "aserai.html"
+}
+function battan(el){
+    window.location.href = "battania.html"
+}
+function sturgia(el){
+    window.location.href = "sturgia.html"
+}
+function imperia(el){
+    window.location.href = "imperia.html"
+}
+function khuzait(el){
+    window.location.href = "khuzait.html"
+}
+function vlandia(el){
+    window.location.href = "vlandia.html"
+}
